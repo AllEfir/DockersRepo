1035c65838752834fd870d8ce76497967ffea871
tehnik <emailexampl@mail>
2021-08-09 21:10:47 +0300

    created script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DockersRepo
-Repo for task
+Solutions to the tasks Docker
+
+![GitHub Logo](https://www.droptica.com/sites/droptica.com/files/styles/blog_banner_image/public/2018-08/docker_codeception-07.jpg?itok=LvsfLy4z)
+
+
+
+Thanks to the Dumynskyi Oleksandr, one of the best person who I know for help me a lot :D
+
--- script.sh
+++ script.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-case $1 in
-  ansible)
-    docker build -t ansible ansible/
-    docker run -v $PWD/ansible/Ansible/:/volumes/Ansible -i ansible:latest
-    echo "ansible container is ready"
-    ;;
-  terraform)
-    docker build -t terraform terraform/
-    docker run -v $PWD/terraform/infrastructure/:/volumes/infrastructure -i terraform:latest
-    echo terraform "container is ready"
-    ;;
-  *)
-    echo dont know, use terraform or ansible
-    ;;
-esac
+echo "Enter the name of the container(ansible or terraform): "
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+        ansible)
+                echo " ansible container be is ready"
+                docker build -t ansible ansible/
+                docker run -v $PWD/ansible/Ansible/:/volumes/Ansible -i ansible:latest
+                echo " ansible container now is ready"
+                break
+                ;;
+        terraform)
+                echo " terraform container be is ready"
+                docker build -t terraform terraform/
+                docker run -v $PWD/terraform/infrastructure/:/volumes/infrastructure
+                echo " terraform container now is ready"
+                break
+                ;;
+        *)
+                echo "Sorry, I don't understand"
+                ;;
+  esac
+done
+echo 
+echo "Goodbye"
+
